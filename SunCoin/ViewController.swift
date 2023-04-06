d48bd146108d1bfa6f708a783c840367c2954468
Mack Fischer <mackfisch@gmail.com>
2023-04-06 16:51:19 -0600

    Created NavigationController, now connects home screen to TipViewController.

--- SunCoin/ViewController.swift
+++ SunCoin/ViewController.swift
@@ -16,52 +16,27 @@ class ViewController: UIViewController {
 
     
 
-    @IBOutlet weak var gameButton: UIButton!
-    @IBOutlet weak var vaultButton: UIButton!
-    @IBOutlet weak var interestButton: UIButton!
-    @IBOutlet weak var flipButton: UIButton!
-    @IBOutlet weak var tipButton: UIButton!
     
-    @IBOutlet weak var homeImage: UIImageView!
-    
-    
-    @IBAction func MenuButtonClicked(_ sender: Any) {
-        MakeMenuInvisible()
-    }
-    
-    @IBAction func HomeImageClicked(_ sender: Any) {
-        MakeMenuVisible()
-    }
+//    @IBOutlet weak var gameButton: UIButton!
+//    @IBOutlet weak var vaultButton: UIButton!
+//    @IBOutlet weak var interestButton: UIButton!
+//    @IBOutlet weak var flipButton: UIButton!
+//    @IBOutlet weak var tipButton: UIButton!
+//
+//    @IBOutlet weak var homeImage: UIImageView!
     
     
-     func MakeMenuInvisible() {
-         gameButton.isEnabled = false
-         flipButton.isEnabled = false
-         tipButton.isEnabled = false
-         interestButton.isEnabled = false
-         vaultButton.isEnabled = false
-         
-         gameButton.isHidden = true
-         flipButton.isHidden = true
-         tipButton.isHidden = true
-         interestButton.isHidden = true
-         vaultButton.isHidden = true
-    }
     
-    func MakeMenuVisible() {
-        gameButton.isEnabled = true
-        flipButton.isEnabled = true
-        tipButton.isEnabled = true
-        interestButton.isEnabled = true
-        vaultButton.isEnabled = true
-        
-        gameButton.isHidden = false
-        flipButton.isHidden = false
-        tipButton.isHidden = false
-        interestButton.isHidden = false
-        vaultButton.isHidden = false
-    }
+
     
     
+    
+    
+    
+    
+    /**
+    let storyBoard : UIStoryboard = UIStoryboard(name: "TipView", bundle: nil)
+    let nextViewController = storyBoard.instantiateViewController(identifier: "nextView") as! nextViewController
+     */
 }
 
---
+++ TipViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TipViewController.swift
+//  SunCoin
+//
+//  Created by MACK FISCHER on 4/6/23.
+//
+
+import UIKit
+
+class TipViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Tip Screen"
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
